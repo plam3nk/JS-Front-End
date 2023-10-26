1ce84791aa9ddf704c82cac28a78a06b244d941a
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-10-27 01:33:37 +0300

    added exercise for js syntax

---
+++ 01. JS Syntax Fundamentals/multiply-the-number-by-2.js
@@ -0,0 +1,5 @@
+function mupltiply_by_2(number) {
+    return console.log(number * 2)
+}
+
+mupltiply_by_2(5)
---
+++ 01. JS Syntax Fundamentals/student-information.js
@@ -0,0 +1,7 @@
+function student_info(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+student_info('John', 15, 5.54678)
+student_info('Steve', 16, 2.1426)
+student_info('Marry', 12, 6.00)
