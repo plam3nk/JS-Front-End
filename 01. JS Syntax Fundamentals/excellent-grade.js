03ee2c1dbd7cd65e00e72fa7a81929b92e475e1b
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-10-28 00:46:11 +0300

    added exercises

---
+++ 01. JS Syntax Fundamentals/excellent-grade.js
@@ -0,0 +1,10 @@
+function excellent_grade(grade) {
+    if (grade < 5.50) {
+        console.log("Not excellent")
+    } else {
+        console.log("Excellent")
+    }
+}
+
+excellent_grade(5.50)
+excellent_grade(4.35)
