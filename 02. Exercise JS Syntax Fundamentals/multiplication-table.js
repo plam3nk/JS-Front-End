3df7d5e869ad14d369f6df1660510e549014bca7
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-11-01 02:58:07 +0200

    added exercise

---
+++ 02. Exercise JS Syntax Fundamentals/multiplication-table.js
@@ -0,0 +1,9 @@
+function multiplication_table(number) {
+    for (let i = 1; i <= 10; i++) {
+        result = number * i
+        console.log(`${number} X ${i} = ${result}`)
+    }
+}
+
+multiplication_table(5)
+multiplication_table(2)
