e40d9fe16f895f5aaf1402fa3a90fec00860c578
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-11-27 17:18:27 +0200

    added exercise

---
+++ 06. Exercise Functions and Statements/password_validator.js
@@ -0,0 +1,28 @@
+function passwordValidator(password) {
+    let invalid = false;
+
+    if (!invalid) {
+        if (!(password.length >=6 && password.length <= 10)) {
+            console.log("Password must be between 6 and 10 characters");
+            invalid = true;
+        }
+
+        if (!/^[a-zA-z0-9]+$/.test(password)) {
+            console.log("Password must consist only of letters and digits");
+            invalid = true;
+        }
+
+        if (!/\d.*\d/.test(password)) {
+            console.log("Password must have at least 2 digits")
+            invalid = true;
+        }
+    }
+
+    if (!invalid) {
+        console.log("Password is valid")
+    }
+}
+
+passwordValidator("logIn")
+passwordValidator("MyPass123")
+passwordValidator("Pa$s$s")
