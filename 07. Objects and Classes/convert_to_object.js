759151f122f997dc8a1e503952fb5053862821c0
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-11-28 14:15:43 +0200

    added exercise

---
+++ 07. Objects and Classes/convert_to_object.js
@@ -0,0 +1,8 @@
+function convertToObject(jsonStr) {
+    let person = JSON.parse(jsonStr)
+    for (const [key,value] of Object.entries(person)){
+        console.log(`${key}: ${value}`)
+    }
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}')
