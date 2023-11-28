c35eb9e8a6e516726c3f037d501c7183a193e228
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-11-28 14:15:02 +0200

    added exercise

---
+++ 06. Exercise Functions and Statements/perfect_number.js
@@ -0,0 +1,13 @@
+function perfectNumber(n) {
+    let sum = 0;
+
+    for (let index = 1; index <= n / 2; index++) {
+        if (n % index === 0) {
+            sum += index;
+        } 
+    }
+
+    sum === n ? console.log("We have a perfect number!") : console.log("It's not so perfect.");
+}
+
+perfectNumber(6)
