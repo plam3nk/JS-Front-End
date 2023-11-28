20a269bf77d7b6d1c716e9e1633e0f3da8d70e97
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-11-28 14:15:08 +0200

    added exercise

---
+++ 06. Exercise Functions and Statements/smallest_of_three.js
@@ -0,0 +1,12 @@
+function smallestNumber(numOne, numTwo, numThree) {
+    let smallestNum = Math.min(numOne, numTwo, numThree)
+
+    console.log(smallestNum)
+    
+}
+
+smallestNumber(2, 5, 3)
+smallestNumber(600,
+    342,
+    123
+    )
