02db717ddad1f7de6e6d598f9ca06aba58a63375
plam3nk <114010915+plam3nk@users.noreply.github.com>
2024-01-03 01:16:36 +0200

    removed zip file

--- 14. Asynchronous Programming/02.Bus-Schedule/app.js
+++ 14. Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,10 +1,37 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const infoSpan = document.getElementsByClassName('info')[0];
+
+    const baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
+    let stopId = 'depot'
+
     function depart() {
-        // TODO: 
+        fetch(`${baseUrl}/${stopId}`)
+            .then(res => res.json())
+            .then(result => {
+                const stopName = result.name;
+
+                infoSpan.textContent = `Next stop ${stopName}`
+
+            })
+            departBtn.setAttribute('disabled', '');
+            arriveBtn.removeAttribute('disabled')
     }
 
     async function arrive() {
-        // TODO:
+        fetch(`${baseUrl}/${stopId}`)
+            .then(res => res.json())
+            .then(result => {
+                const nextStopId = result.next;
+                const stopName = result.name;
+
+                infoSpan.textContent = `Arriving at ${stopName}`
+
+                stopId = nextStopId;
+            })
+            arriveBtn.setAttribute('disabled', '');
+            departBtn.removeAttribute('disabled')
     }
 
     return {
