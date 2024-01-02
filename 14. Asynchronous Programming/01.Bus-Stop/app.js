b157015e0a7692ea3cc9c5bb39fe53cbaa3bf152
plam3nk <114010915+plam3nk@users.noreply.github.com>
2024-01-03 01:14:45 +0200

    added exercise

--- 14. Asynchronous Programming/01.Bus-Stop/app.js
+++ 14. Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,21 @@
 function getInfo() {
-    // TODO:
+    const baseUrl = 'http://localhost:3030/jsonstore/bus/businfo';
+    const stopIdInput = document.getElementById('stopId');
+    const stopNameElement = document.getElementById('stopName');
+    const busesListElement = document.getElementById('buses');
+
+    fetch(`${baseUrl}/${stopIdInput.value}`)
+        .then(res => res.json())
+        .then(result => {
+            stopNameElement.textContent = result.name;
+            const buses = result.buses;
+            for (const [busId, time] of Object.entries(buses)) {
+                listItem = document.createElement('li');
+                listItem.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                busesListElement.appendChild(listItem)
+            }
+        })
+        .catch(err => {
+            stopNameElement.textContent = 'Error';
+        })
 }
